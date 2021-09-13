6528eab499c3da88944c0c02282b1c72e3bbe8fe
Gustavo Henrique Cielo <gh.cielo@hotmail.com>
2021-09-13 11:21:17 -0300

    fix: rec-dot for slider showing correct color

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -27,18 +27,26 @@ export const SectionCarousel = styled.section`
       color: #ffffff;
     }
   }
+  
   div {
     display: flex;
   }
+
   .keyContainer {
     height: 700px;
   }
+
   button.rec-dot {
     background-color: #c4c4c4;
     box-shadow: 0 0 1px 3px #c4c4c4;
     width: 29px;
     height: 29px;
     margin-top: 33px;
+  }
+
+  button.rec-dot_active {
+    box-shadow: 0 0 1px 3px #4ac959;
+    background-color: #4ac959;
   }
 
   button.rec-dot:hover,
